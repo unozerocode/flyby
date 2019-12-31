6b0b6886fe2d0ad164eb5fdb111bdb62ecd4a453
unozerocode <enrique@unozerocode.com>
2019-12-31 17:00:12 -0600

    step6

--- lib/main.dart
+++ lib/main.dart
@@ -33,9 +33,39 @@ class RandomWordsState extends State<RandomWords> {
 
   @override 
   Widget build(BuildContext context){
-    final WordPair wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(appBar: AppBar(
+      title: Text("Startup Name Geneerator")
+    ),
+    body: _buildSuggestions());
   }
-}
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i){
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        final int index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont
+      ),
+    );
+  }
+}  // RandomWordsState
 
 
